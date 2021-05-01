f0da51016a51a7419a63548e8eaf11d80e744a8d
JoaoDiogoMesquita <joao.diogo106@gmail.com>
2021-05-01 14:33:28 +0100

    .

--- Angle Detector Double Marker/angle_detector_double.js
+++ Angle Detector Double Marker/angle_detector_double.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('mt-angle-detector-double', {
+AFRAME.registerComponent('mt-angle-detector-dm', {
   schema: {
     secondMarker : {type: 'selector'},
     threshold : {type: 'float', default: 20},
@@ -7,7 +7,7 @@ AFRAME.registerComponent('mt-angle-detector-double', {
   },
 
   init: function () {
-    console.log("Initializing mt-angle detector component double");
+    console.log("Initializing mt-angle detector component double marker");
   },
 
 
@@ -25,38 +25,43 @@ AFRAME.registerComponent('mt-angle-detector-double', {
 
 
   tick: function (time) {
-    this.plane1.normal.set(0, 1, 0).applyQuaternion(this.firstMarker.object3D.quaternion);
-    this.plane2.normal.set(0, -1, 0).applyQuaternion(this.secondMarker.object3D.quaternion);
-    this.angle =  THREE.Math.radToDeg(this.plane1.normal.angleTo(this.plane2.normal));
+    if(this.firstMarker.object3D.visible && this.secondMarker.object3D.visible ) {
+      this.plane1.normal.set(0, 1, 0).applyQuaternion(this.firstMarker.object3D.quaternion);
+      this.plane2.normal.set(0, -1, 0).applyQuaternion(this.secondMarker.object3D.quaternion);
+      this.angle = THREE.Math.radToDeg(this.plane1.normal.angleTo(this.plane2.normal));
 
-    for (let i = 0; i <= 360 ; i += this.data.threshold) {
-
-      if (i > this.angle) {
-        var level = i-this.data.threshold;
-        if ( level != this.lastAngle) {
-          this.lastAngle = level
-          const event_rotation_double = new CustomEvent('event_rotation_double', {
-            detail: {
-              time: time,
-              object1: this.el,
-              object2: this.secondMarker,
-              angle: level
-            },
-          });
-          this.el.dispatchEvent(event_rotation_double);
-          console.log('Emitting event', event_rotation_double, ' : Angle of', level, 'between ', this.firstMarker, ' and ', this.secondMarker, ' to:<', this.el, '>');
-
-          if (this.data.eventTargets != null) {
-            this.data.eventTargets.forEach(function (target) {
-              target.dispatchEvent(event_rotation_double);
-              console.log('Emitting event', event_rotation_double, ' : Angle of',  level, 'between ', this.firstMarker, ' and ', this.secondMarker, ' to:<', target, '>');
-            }.bind(this))
+      for (let i = 0; i <= 360; i += this.data.threshold) {
+        if (i > this.angle) {
+          var level = i - this.data.threshold;
+          if (level != this.lastAngle) {
+            this.lastAngle = level
+            this.send_event(time, level)
           }
+          break
         }
-        break
       }
     }
   },
+
+  send_event: (function( time, level) {
+    const event_rotation_double = new CustomEvent('event_rotation_dm', {
+      detail: {
+        time: time,
+        object1: this.el,
+        object2: this.secondMarker,
+        angle: level
+      },
+    });
+    this.el.dispatchEvent(event_rotation_double);
+    console.log('Emitting event', event_rotation_double, ' : Angle of', level, 'between ', this.firstMarker, ' and ', this.secondMarker, ' to:<', this.el, '>');
+
+    if (this.data.eventTargets != null) {
+      this.data.eventTargets.forEach(function (target) {
+        target.dispatchEvent(event_rotation_double);
+        console.log('Emitting event', event_rotation_double, ' : Angle of',  level, 'between ', this.firstMarker, ' and ', this.secondMarker, ' to:<', target, '>');
+      }.bind(this))
+    }
+  }),
 
 });
 
---
+++ Angle Detector Single Marker/angle_detector_single.js
@@ -0,0 +1,131 @@
+AFRAME.registerComponent('mt-angle-detector-sm', {
+  schema: {
+    threshold : {type: 'float', default: 20},
+    eventTargets: {type: 'selectorAll'},
+    axis : {type: 'array', default: ['x','y','z']},
+    debug : {type: 'boolean', default: false}
+  },
+
+  init: function () {
+    console.log("Initializing mt-angle detector component");
+  },
+
+  update: function(){
+    this.firstIteration = true;
+    this.lastAngle = new THREE.Vector3();
+    this.actualAngle= new THREE.Vector3();
+    this.inputAngle = new THREE.Vector3();
+    this.savedAngle = new THREE.Vector3();
+    this.plane = new THREE.Plane();
+    this.threshold = this.data.threshold;
+    console.info("Debug mode set to ", this.data.debug)
+  },
+
+
+
+  tick: function (time, timeDelta) {
+    if (this.el.object3D.visible) {
+      //Ignore first rotation, because of the agressive change in coordenates from 0,0,0 to first position
+      if (this.firstIteration) {
+        this.firstIteration = false;
+        this.lastAngle.x = this.el.object3D.rotation.x
+        this.lastAngle.y = this.el.object3D.rotation.y
+        this.lastAngle.z = this.el.object3D.rotation.z
+      } else {
+        this.lastAngle.copy(this.actualAngle)
+      }
+
+      //Actual position
+      this.actualAngle.x = this.el.object3D.rotation.x
+      this.actualAngle.y = this.el.object3D.rotation.y
+      this.actualAngle.z = this.el.object3D.rotation.z
+
+
+      //Process only in the selected axes for optimal perfomance
+      this.data.axis.forEach(function (axis) {
+            //console.log( THREE.Math.radToDeg(this.actualAngle[axis]), this.actualAngle[axis])
+
+
+            //Calculate input angle
+
+            //Both positive side
+            if (this.actualAngle[axis] > 0 && this.lastAngle[axis] > 0) {
+              this.inputAngle[axis] = this.actualAngle[axis] - this.lastAngle[axis]
+            }
+            //Both negative side
+            else if (this.actualAngle[axis] < 0 && this.lastAngle[axis] < 0) {
+              this.inputAngle[axis] = Math.abs(this.actualAngle[axis]) - Math.abs(this.lastAngle[axis])
+            }
+
+            //Transition from positive side to negative
+            else if (this.actualAngle[axis] < 0 && this.lastAngle[axis] > 0) {
+              //  -> -pi <- pi
+              if (Math.abs(this.actualAngle[axis]) > Math.PI / 2) {
+                this.inputAngle[axis] = (Math.PI - Math.abs(this.actualAngle[axis])) + (Math.PI - this.lastAngle[axis])
+              }
+              // -0.1 <- 0.1
+              else {
+                this.inputAngle[axis] = Math.abs(this.actualAngle[axis]) + this.lastAngle[axis]
+              }
+            }
+
+            //Transition from negative side to positive
+            else if (this.actualAngle[axis] > 0 && this.lastAngle[axis] < 0) {
+              //-pi -> pi
+              if (Math.abs(this.actualAngle[axis]) > Math.PI / 2) {
+                this.inputAngle[axis] = (Math.PI - this.actualAngle[axis]) + (Math.PI - Math.abs(this.lastAngle[axis]))
+              }
+              //-0.1 -> 0.1
+              else {
+                this.inputAngle[axis] = this.actualAngle[axis] + Math.abs(this.lastAngle[axis])
+              }
+            }
+
+            this.savedAngle[axis] += this.inputAngle[axis];
+
+            if (this.data.debug)
+              console.log('ATUAL', this.actualAngle[axis] , '\t\t LAST', THREE.Math.radToDeg(this.lastAngle[axis]), '\t\t input', THREE.Math.radToDeg(this.inputAngle[axis]), '\t\t SAVED  ', THREE.Math.radToDeg(this.savedAngle[axis]))
+
+            //Has saved angle passed the threshold ?
+            if (Math.abs(this.savedAngle[axis]) >= THREE.Math.degToRad(this.threshold)) {
+              //Emit events to this object and targets
+              let direction = ''
+              if (this.savedAngle[axis] > 0) {
+                direction = 'positive';
+              } else {
+                direction = 'negative';
+              }
+              const event_rotation = new CustomEvent('event_rotation', {
+                detail: {
+                  time: time,
+                  axis: axis,
+                  direction: direction,
+                  object: this.el,
+                  threshold: this.threshold
+                },
+              });
+
+              this.el.dispatchEvent(event_rotation)
+
+              if (this.data.eventTargets != null) {
+                this.data.eventTargets.forEach(function (target) {
+                  target.dispatchEvent(event_rotation);
+                  console.log('Emitting event', event_rotation, ' : Rotation of', this.data.threshold, 'degrees (', THREE.Math.degToRad(this.data.threshold), ' rad) in ', axis, ' to:<', target, '>');
+                }.bind(this))
+              }
+              // Restart saved angle variable
+              if(this.savedAngle[axis] > 0) {
+                this.savedAngle[axis] = this.savedAngle[axis] - THREE.Math.degToRad(this.threshold);
+              }
+              else if(this.savedAngle[axis] < 0){
+                this.savedAngle[axis] = this.savedAngle[axis] + THREE.Math.degToRad(this.threshold);
+              }
+            }
+
+          }.bind(this)
+      )
+    }
+  }
+
+});
+
