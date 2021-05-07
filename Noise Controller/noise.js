65141941e33669f4c35b215d6a0359ef4c7cfdb0
JoaoDiogoMesquita <joao.diogo106@gmail.com>
2021-05-07 21:34:10 +0100

    Algorithm update

--- Noise Controller/noise.js
+++ Noise Controller/noise.js
@@ -1,35 +1,53 @@
 AFRAME.registerComponent("mt-noise-controller", {
     schema: {
-        maximum_time: { type: 'int', default: 1000 },
-        debug : {type: 'boolean', default: false},
+        maximum_time: {type: 'int', default: 1000},
+        debug: {type: 'boolean', default: false},
     },
 
     init: function () {
         console.log("Initializing mt-noise-controller");
         this.visible = undefined;
+        this.firstLoss = true;
 
-        this.el.addEventListener("markerFound", function() {
-            if(this.data.debug)
-                console.log("Marker ", this.el , " visible.");
+
+        this.el.addEventListener("markerFound", function () {
+
+            clearTimeout(this.timeout);
             this.visible = true;
+            this.firstLoss = true;
+
+            if (this.data.debug) {
+                console.log("MARKER FOUND.");
+                console.log("Timeout clear.");
+            }
         }.bind(this));
 
-
-        this.el.addEventListener("markerLost", function() {
-
-            if(this.data.debug)
-                console.log("Marker ", this.el , " lost.");
-
-            this.visible = false;
+        this.el.addEventListener("markerLost", function () {
             this.el.object3D.visible = true;
-
-            setTimeout(() => {
-                if(this.visible == false){
-                    this.el.object3D.visible = false;
-                }
-            }, this.data.maximum_time);
-
+            this.visible = false;
         }.bind(this));
     },
 
+
+    tick: function () {
+
+        if(this.visible == false && this.firstLoss == true){
+
+            this.timeout = setTimeout(() => {
+                if (this.visible == false) {
+                    this.el.object3D.visible = false;
+                    if(this.data.debug)
+                        console.log("Marker scene gone.")
+                }
+            }, this.data.maximum_time);
+
+            if (this.data.debug)
+                console.log("MARKER LOST.");
+
+            this.firstLoss = false;
+        }
+
+
+
+    },
 });
